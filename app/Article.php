446416c18ee61223c22713e87749b5eaed57de41
shigeyoshi.koyama <shigeyoshi.koyama@zozo.com>
2025-11-17 18:24:10 +0900

    workflows test.

--- app/Article.php
+++ app/Article.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
 class Article extends Model
 {
     protected $fillable = [
