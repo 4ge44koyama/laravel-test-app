be93f1f2795bf46aa738e86ea6e607acbe07ec56
shigeyoshi.koyama <shigeyoshi.koyama@zozo.com>
2025-11-17 18:15:08 +0900

    workflows test.

--- app/Article.php
+++ app/Article.php
@@ -1,25 +1,24 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
 class Article extends Model
 {
+
     protected $fillable = [
         'title',
         'body',
     ];
     
-    public function user(): BelongsTo
+    public function user():BelongsTo
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User');    
     }
 
     public function likes(): BelongsToMany
     {
+        
         return $this->belongsToMany('App\User', 'likes')->withTimestamps();
     }
 
